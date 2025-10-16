cba8632d27ad321993f85d91d65c5754a3198672
cwh090523 <cwh090523@gmail.com>
2025-10-16 17:36:13 +0900

    feat: asdhjbvefhou

--- Assets/CWH/Scripts/Fade.cs
+++ Assets/CWH/Scripts/Fade.cs
@@ -2,89 +2,101 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
-using static UnityEngine.Rendering.DebugUI;
 
-public class Fade : MonoBehaviour
+namespace CWH
 {
-    public bool IsFadeIn;
-    public Image image;
 
-    private void Start()
+    public class Fade : MonoBehaviour
     {
-        if (IsFadeIn)
-        {
-            image.gameObject.SetActive(true);
-            StartCoroutine(CoFadeIn());
-        }
-        else
-        {
-            image.gameObject.SetActive(false);
-        }
-    }
-    private void Update()
-    {
-        if (Keyboard.current.kKey.wasPressedThisFrame)
+        public bool IsFadeIn =false;
+        public Image image;
+        public Image background;
+        public Sprite[] sprites; // 다음 이미지 목록
+        private int currentIndex = 0;
+
+        private void Start()
         {
             if (IsFadeIn)
             {
-                FadeIn();
+                image.gameObject.SetActive(true);
+                StartCoroutine(CoFadeIn());
             }
             else
             {
-                FadeOut();
+                image.gameObject.SetActive(false);
             }
         }
-    }
-    public void FadeOut()
-    {
-        image.gameObject.SetActive(true); // Panel 활성화
-        Debug.Log("FadeCanvasController_ Fade Out 시작");
-        StartCoroutine(CoFadeOut());
-        Debug.Log("FadeCanvasController_ Fade Out 끝");
-        IsFadeIn = true;
-    }
-    public void FadeIn()
-    {
-        image.gameObject.SetActive(true); // Panel 활성화
-        Debug.Log("FadeCanvasController_ Fade Out 시작");
-        StartCoroutine(CoFadeIn());
-        Debug.Log("FadeCanvasController_ Fade Out 끝");
-        IsFadeIn = false;
-    }
-    IEnumerator CoFadeIn()
-    {
-        float elapsedTime = 0f; // 누적 경과 시간
-        float fadedTime = 1f; // 총 소요 시간
 
-        while (elapsedTime <= fadedTime)
+        private void Update()
         {
-            image.GetComponent<CanvasRenderer>().SetAlpha(Mathf.Lerp(1f, 0f, elapsedTime / fadedTime));
-
-            elapsedTime += Time.deltaTime;
-            Debug.Log("Fade In 중...");
-            yield return null;
-        }
-        Debug.Log("Fade In 끝");
-        image.gameObject.SetActive(false); // Panel을 비활성화
-        yield break;
-    }
-    IEnumerator CoFadeOut()
-    {
-        float elapsedTime = 0f; // 누적 경과 시간
-        float fadedTime = 1f; // 총 소요 시간
-
-        while (elapsedTime <= fadedTime)
-        {
-            image.GetComponent<CanvasRenderer>().SetAlpha(Mathf.Lerp(0f, 1f, elapsedTime / fadedTime));
-
-            elapsedTime += Time.deltaTime;
-            Debug.Log("Fade Out 중...");
-            yield return null;
+            if (Keyboard.current.kKey.wasPressedThisFrame)
+            {
+                if (IsFadeIn)
+                    FadeOut();
+                else
+                    FadeIn();
+            }
         }
 
-        Debug.Log("Fade Out 끝");
-        image.gameObject.SetActive(false);
-        yield break;
+        public void FadeOut()
+        {
+            image.gameObject.SetActive(true);
+            Debug.Log("FadeCanvasController_ Fade Out 시작");
+            StartCoroutine(CoFadeOut());
+            IsFadeIn = false;
+        }
+
+        public void FadeIn()
+        {
+            image.gameObject.SetActive(true);
+            Debug.Log("FadeCanvasController_ Fade In 시작");
+            StartCoroutine(CoFadeIn());
+            IsFadeIn = true;
+        }
+
+        IEnumerator CoFadeIn()
+        {
+            float elapsedTime = 0f;
+            float fadedTime = 1f;
+
+            image.canvasRenderer.SetAlpha(0f);
+
+            while (elapsedTime <= fadedTime)
+            {
+                image.canvasRenderer.SetAlpha(Mathf.Lerp(0f, 1f, elapsedTime / fadedTime));
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            image.canvasRenderer.SetAlpha(1f);
+            Debug.Log("Fade In 끝");
+
+            // 페이드인 완료 후 다음 이미지로 변경
+            currentIndex = (currentIndex + 1) % sprites.Length;
+            background.sprite = sprites[currentIndex];
+
+            yield break;
+        }
+
+        IEnumerator CoFadeOut()
+        {
+            float elapsedTime = 0f;
+            float fadedTime = 1f;
+
+            image.canvasRenderer.SetAlpha(1f);
+
+            while (elapsedTime <= fadedTime)
+            {
+                image.canvasRenderer.SetAlpha(Mathf.Lerp(1f, 0f, elapsedTime / fadedTime));
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            image.canvasRenderer.SetAlpha(0f);
+            Debug.Log("Fade Out 끝");
+            image.gameObject.SetActive(false);
+            yield break;
+        }
     }
 
 }
